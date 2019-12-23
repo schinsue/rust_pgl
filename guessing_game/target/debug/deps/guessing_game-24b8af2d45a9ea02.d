68c043dcb828073cc4d035b4ce3059d1c8e24c76
Sergio Chin-Sue <s.chinsue@gmail.com>
2019-12-23 18:27:25 +0100

    C2 part 1

---
+++ guessing_game/target/debug/deps/guessing_game-24b8af2d45a9ea02.d
@@ -0,0 +1,5 @@
+/home/amplifix/projects/rust/guessing_game/target/debug/deps/guessing_game-24b8af2d45a9ea02.rmeta: src/main.rs
+
+/home/amplifix/projects/rust/guessing_game/target/debug/deps/guessing_game-24b8af2d45a9ea02.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/amplifix/projects/rust/guessing_game/target/debug/guessing_game: /home/amplifix/projects/rust/guessing_game/src/main.rs
