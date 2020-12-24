c9b92b6510a04949dc19d37b8dd6b028a41719f5
Sergio Chin-Sue <s.chinsue@gmail.com>
2020-12-24 16:19:01 +0000

    C3 - Variables and mutability

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/sergiorachid/Projects/rust_pgl/variables/target/debug/deps/variables: src/main.rs
+
+/Users/sergiorachid/Projects/rust_pgl/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/sergiorachid/Projects/rust_pgl/variables/target/debug/variables: /Users/sergiorachid/Projects/rust_pgl/variables/src/main.rs
