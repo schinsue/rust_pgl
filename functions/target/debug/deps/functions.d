ab54c9ec3ebfdb8585063bca11701af35422df0a
Sergio Chin-Sue <s.chinsue@gmail.com>
2020-12-26 14:55:52 +0000

    C3 - Functions

---
+++ functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+/Users/sergiorachid/Projects/rust_pgl/functions/target/debug/deps/functions: src/main.rs
+
+/Users/sergiorachid/Projects/rust_pgl/functions/target/debug/deps/functions.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/sergiorachid/Projects/rust_pgl/functions/target/debug/functions: /Users/sergiorachid/Projects/rust_pgl/functions/src/main.rs
