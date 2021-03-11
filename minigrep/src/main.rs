2d9a6c0aea0508358d9f143fc1092c18c5c09d64
Sergio Chin-Sue <s.chinsue@gmail.com>
2021-03-11 03:12:21 +0000

    C13 - Improvements

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,8 +4,13 @@ use std::process;
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    // let args: Vec<String> = env::args().collect();
+    // let config = Config::new(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {}", err);
+    //     process::exit(1);
+    // });
+
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
